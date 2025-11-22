7526e03d0e1f66821f57203c1073b9d839632162
ekagra <ekagranigam@gmail.com>
2025-11-22 14:36:56 +0530

    works with docker compose postgres

--- Backend/screenlit/src/main/java/com/ekagra/screenlit/ScreenlitApplication.java
+++ Backend/screenlit/src/main/java/com/ekagra/screenlit/ScreenlitApplication.java
@@ -3,12 +3,14 @@ package com.ekagra.screenlit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ScreenlitApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(ScreenlitApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+        SpringApplication.run(ScreenlitApplication.class, args);
 	}
 
 }
